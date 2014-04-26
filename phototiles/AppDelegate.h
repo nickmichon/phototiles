999a9c6d19439ef776498ab8e912eb7731e15d31
Mikolaj Michon <nickmichon@gmail.com>
2014-04-26 15:33:05 -0400

    Initial Commit

---
+++ phototiles/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  phototiles
+//
+//  Created by Mikolaj Michon on 2014-04-26.
+//  Copyright (c) 2014 Mikolaj Michon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ phototiles/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  phototiles
+//
+//  Created by Mikolaj Michon on 2014-04-26.
+//  Copyright (c) 2014 Mikolaj Michon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
